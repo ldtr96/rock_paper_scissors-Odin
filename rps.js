5a0b9229181cb4d76e7d05bbcb98bc5342082239
ldtr96 <129347909+ldtr96@users.noreply.github.com>
2025-06-28 13:00:10 -0400

    Added more formatting an styling to the UI.

--- rps.js
+++ rps.js
@@ -9,18 +9,6 @@ function getComputerChoice(){
     return cpuchoice;  
 }
 
-
-
-/*function getHumanChoice(){
-    let userprompt = prompt("Please enter 'rock','paper', or 'scissors'.");
-    userprompt = userprompt.toLowerCase();
-    if (userprompt === "rock"){
-        userchoice = "rock"
-    } else if (userprompt ==="paper"){
-        userchoice = "paper"
-    } else userchoice = "scissors";
-    return userchoice;
-}*/
 let humanScore = 0; 
 let computerScore = 0;
 choicerock = document.querySelector("#rock");
@@ -36,46 +24,28 @@ btn.forEach(btn => {
                 }     
                 function playRound(userchoice,cpuchoice){
             if (userchoice==="rock" && cpuchoice==="rock"){
-                console.log("Computer chose: rock")
-                console.log("You chose: rock")
                 console.log("It's a tie! You both selected rock.")
             } else if (userchoice==="rock" && cpuchoice==="paper"){
-                console.log("Computer chose: paper")
-                console.log("You chose: rock")
                 console.log("Round lost! Paper beats rock.")
                 computerScore+=1
             } else if (userchoice==="rock" && cpuchoice==="scissors"){
-                console.log("Computer chose: scissors")
-                console.log("You chose: rock")
                 console.log("Round won! Rock beats scissors.")
                 humanScore+=1
             } else if (userchoice==="paper" && cpuchoice==="rock"){
-                console.log("Computer chose: rock")
-                console.log("You chose: paper")
                 console.log("Round won! Paper beats rock.")
                 humanScore+=1
             } else if (userchoice==="paper" && cpuchoice==="paper"){
-                console.log("Computer chose: paper")
-                console.log("You chose: paper")
                 console.log("It's a tie! You both selected paper.")
             } else if (userchoice==="paper" && cpuchoice==="scissors"){
-                console.log("Computer chose: scissors")
-                console.log("You chose: paper")
                 console.log("Round lost! Scissors beats paper.")
                 computerScore+=1
             } else if (userchoice==="scissors" && cpuchoice==="rock"){
-                console.log("Computer chose: rock")
-                console.log("you chose: scissors")
                 console.log("Round lost! Rock beats scissors.")
                 computerScore+=1
             } else if (userchoice==="scissors" && cpuchoice==="paper"){
-                console.log("Computer chose: paper")
-                console.log("You chose: scissors")
                 console.log("Round won! Scissors beats paper.")
                 humanScore+=1
             } else if (userchoice==="scissors" && cpuchoice==="scissors"){
-                console.log("Computer chose: scissors")
-                console.log("You chose: scissors")
                 console.log("It's a tie! You both selected scissors.")
             }
 
@@ -83,9 +53,50 @@ btn.forEach(btn => {
                 const humanSelection = getHumanChoice();
                 const computerSelection = getComputerChoice();
                 playRound(humanSelection,computerSelection);
-                console.log("The score is computer: "+computerScore+ " you: "+humanScore);
-                computerScore;
-                humanScore;
+                roundScore = "The score is computer: "+computerScore+ " you: "+humanScore;
+                roundchoices = "Computer chose: " + cpuchoice + "." + "You chose: " + userchoice + ".";
+                
+                newcreatediv = document.createElement("div");
+                newcreatediv.setAttribute("id", "result-div");
+                
+                
+                scorediv = document.createElement("div");
+                scorediv.setAttribute("id", "score-div");
+                scorediv.textContent = roundScore;
+
+                choicesdiv = document.createElement("div");
+                choicesdiv.setAttribute("id", "choices-div");
+                choicesdiv.textContent = roundchoices;
+
+                newcreatediv.appendChild(choicesdiv);
+                newcreatediv.appendChild(scorediv);
+
+                resultsParent = document.querySelector("#results-window")
+                
+                oldcreatediv = document.querySelector("#result-div")
+
+                if (oldcreatediv == null){
+                    resultsParent.appendChild(newcreatediv);
+                }
+
+                else {
+                    oldcreatediv.replaceWith(newcreatediv);
+                    resultsParent.appendChild(newcreatediv);
+                }
+
+                if (humanScore == 5){
+                    scorediv.textContent = "You're a winner and all you do is win!"
+                    humanScore = 0
+                    computerScore = 0
+                    window.open("https://www.youtube.com/watch?v=GGXzlRoNtHU&list=RDGGXzlRoNtHU&start_radio=1");
+                }
+                
+                else if (computerScore ==5){
+                    scorediv.textContent ="You lost?!? What a loser..."
+                    humanScore = 0
+                    computerScore = 0
+                    window.open("https://www.youtube.com/watch?v=dQw4w9WgXcQ&list=RDdQw4w9WgXcQ&start_radio=1");
+                }
 
             }
         )  
@@ -94,90 +105,6 @@ btn.forEach(btn => {
 
 
 
-/*
-btn.addEventlistener("click", ()=>{
-    if 
-})
-
-function getHumanChoice2(){
-    choicerock.addEventListener("click",()=>{
-        userchoice = "rock";
-        return userchoice;
-        }
-    )
-
-    choicepaper.addEventListener("click",()=>{
-        userchoice = "paper";
-        return userchoice;
-        }
-    )
-
-    choicescissors.addEventListener("click", () =>{
-        userchoice = "scissors";
-        return userchoice;
-    })
-
-} */
-
-
-
-
-function playGame2(){      
-    function playRound(userchoice,cpuchoice){
-            if (userchoice==="rock" && cpuchoice==="rock"){
-                console.log("Computer chose: rock")
-                console.log("You chose: rock")
-                console.log("It's a tie! You both selected rock.")
-            } else if (userchoice==="rock" && cpuchoice==="paper"){
-                console.log("Computer chose: paper")
-                console.log("You chose: rock")
-                console.log("Round lost! Paper beats rock.")
-                computerScore+=1
-            } else if (userchoice==="rock" && cpuchoice==="scissors"){
-                console.log("Computer chose: scissors")
-                console.log("You chose: rock")
-                console.log("Round won! Rock beats scissors.")
-                humanScore+=1
-            } else if (userchoice==="paper" && cpuchoice==="rock"){
-                console.log("Computer chose: rock")
-                console.log("You chose: paper")
-                console.log("Round won! Paper beats rock.")
-                humanScore+=1
-            } else if (userchoice==="paper" && cpuchoice==="paper"){
-                console.log("Computer chose: paper")
-                console.log("You chose: paper")
-                console.log("It's a tie! You both selected paper.")
-            } else if (userchoice==="paper" && cpuchoice==="scissors"){
-                console.log("Computer chose: scissors")
-                console.log("You chose: paper")
-                console.log("Round lost! Scissors beats paper.")
-                computerScore+=1
-            } else if (userchoice==="scissors" && cpuchoice==="rock"){
-                console.log("Computer chose: rock")
-                console.log("you chose: scissors")
-                console.log("Round lost! Rock beats scissors.")
-                computerScore+=1
-            } else if (userchoice==="scissors" && cpuchoice==="paper"){
-                console.log("Computer chose: paper")
-                console.log("You chose: scissors")
-                console.log("Round won! Scissors beats paper.")
-                humanScore+=1
-            } else if (userchoice==="scissors" && cpuchoice==="scissors"){
-                console.log("Computer chose: scissors")
-                console.log("You chose: scissors")
-                console.log("It's a tie! You both selected scissors.")
-            }
-        
-    }
-
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-
-        playRound(humanSelection,computerSelection);
-        console.log("The score is computer: "+computerScore+ " you: "+humanScore);
-        return computerScore;
-        return humanScore;
-}
 
 
 //The below is outside of functions. Comment out to avoid the script automatically running for now.
